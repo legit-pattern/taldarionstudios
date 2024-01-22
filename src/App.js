0ebcf91fe3227312b1e6f2e088b19c19822b733b
Mathias Ögärd <mathias.ogard@sigmatechnology.se>
2024-01-22 18:25:09 +0100

    third

--- src/App.js
+++ src/App.js
@@ -1,63 +1,34 @@
 import './App.css';
-import { Box, List, Grid, ListItemButton, ListItemText, Typography } from '@mui/material';
+import { Box, Button, Stack, Container, Typography } from '@mui/material';
+// import header from "./public/header-1.png";
 
 function App() {
   return (
-    <Box style={{ display: "flex", flexDirection: "column" }}>
+    <Container style={{ display: "flex", flexDirection: "column" }}>
+    <Box>
+      <img src="/banner-1.jpeg" alt="Banner" style={{ width: '100%', height: 'auto', display: 'block' }}/>
+    </Box>
 
-    <Grid item xs={12} md={6} style={{ paddingRight: "10px", paddingLeft: "40px" }}>
-      <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+    <Box style={{display: "flex", flexDirection: "column", alignSelf: "center" }}>
+      <Typography sx={{ mt: 4, mb: 2 }} variant="h4" component="div" style={{ paddingLeft: "18px", color: "#ECDCB1", fontFamily: "helvetica", alignSelf: "center" }}>
         Education
       </Typography>
-        <List dense={true} style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
-          <ListItemButton target='_blank' href='https://chat.openai.com/g/g-wTI7lRowq-lrne-homework-helper'>
-            <ListItemText
-              primary="Homework helper"
-              />
-          </ListItemButton>
+      <Stack spacing={2} direction={"row"} >
+        <Button variant="contained" target='_blank' href='https://chat.openai.com/g/g-wTI7lRowq-lrne-homework-helper' style={{ fontFamily: "monospace", color: "black", background: "#ECDCB1", margin: "10px" }}>
+          <Typography variant='h7'>Homework helper</Typography>
+        </Button>
 
-          <ListItemButton target='_blank' href='https://chat.openai.com/g/g-kLXy5ZCqD-logo-animator'>
-            <ListItemText
-              primary="Logo Animator"
-              />
-          </ListItemButton>
+        <Button variant="contained" target='_blank' href='https://chat.openai.com/g/g-kLXy5ZCqD-logo-animator' style={{ fontFamily: "monospace", color: "black", background: "#ECDCB1", margin: "10px" }}>
+          <Typography variant='h7'>Logo Animator</Typography>
+        </Button>
 
-          <ListItemButton target='_blank' href='https://chat.openai.com/g/g-vfxEmHLfH-store-climber'>
-            <ListItemText
-              primary="Store Climber"
-              />
-          </ListItemButton>
-
-        </List>
-    </Grid>
-
-    {/* <Grid item xs={12} md={6} style={{ paddingRight: "10px", paddingLeft: "40px" }}>
-      <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-        Lifestyle
-      </Typography>
-      <List dense={true} style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
-        <ListItemButton target='_blank' href='https://chat.openai.com/g/g-wTI7lRowq-lrne-homework-helper'>
-          <ListItemText
-            primary="Homework helper"
-            />
-        </ListItemButton>
-
-        <ListItemButton target='_blank' href='https://chat.openai.com/g/g-kLXy5ZCqD-logo-animator'>
-          <ListItemText
-            primary="Logo Animator"
-            />
-        </ListItemButton>
-
-        <ListItemButton target='_blank' href='https://chat.openai.com/g/g-vfxEmHLfH-store-climber'>
-          <ListItemText
-            primary="Store Climber"
-            />
-        </ListItemButton>
-
-      </List>
-    </Grid> */}
-
+        <Button variant="contained" target='_blank' href='https://chat.openai.com/g/g-vfxEmHLfH-store-climber' style={{ fontFamily: "monospace", color: "black", background: "#ECDCB1", margin: "10px" }}>
+          <Typography variant='h7'>Store Climber</Typography>
+        </Button>
+      </Stack>
     </Box>
+
+    </Container>
   );
 }
 
