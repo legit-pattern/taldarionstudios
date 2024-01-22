896201c2418cf9ea0bf131a7a5c293f6188f7911
Mathias Ögärd <mathias.ogard@sigmatechnology.se>
2024-01-22 19:04:14 +0100

    add header and footer, customer is pleased

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import './App.css';
 import { Box, Button, Stack, Container, Typography } from '@mui/material';
-// import header from "./public/header-1.png";
 
 function App() {
   return (
+    <Container>
+    {/* <img src="/header.png" alt="Banner" style={{ width: '100%', height: 'auto', display: 'block' }}/> */}
+
+    <div style={{ width: '100%' }}>
+      <img src="/header.png" alt="Banner" style={{ width: '100%', height: 'auto', display: 'block' }}/>
+    </div>
+    
     <Container style={{ display: "flex", flexDirection: "column" }}>
-    <Box>
       <img src="/banner-1.jpeg" alt="Banner" style={{ width: '100%', height: 'auto', display: 'block' }}/>
-    </Box>
 
     <Box style={{display: "flex", flexDirection: "column", alignSelf: "center" }}>
-      <Typography sx={{ mt: 4, mb: 2 }} variant="h4" component="div" style={{ paddingLeft: "18px", color: "#ECDCB1", fontFamily: "helvetica", alignSelf: "center" }}>
-        Education
+      <Typography sx={{ mt: 4, mb: 2 }} variant="h4" component="div" style={{ color: "#ECDCB1", fontFamily: "tektur", alignSelf: "center" }}>
+        EDUCATION
       </Typography>
       <Stack spacing={2} direction={"row"} >
-        <Button variant="contained" target='_blank' href='https://chat.openai.com/g/g-wTI7lRowq-lrne-homework-helper' style={{ fontFamily: "monospace", color: "black", background: "#ECDCB1", margin: "10px" }}>
-          <Typography variant='h7'>Homework helper</Typography>
+        <Button variant="contained" target='_blank' href='https://chat.openai.com/g/g-wTI7lRowq-lrne-homework-helper' style={{ color: "black", background: "#ECDCB1", margin: "10px" }}>
+          <Typography variant='h6' style={{ fontFamily: "tektur" }}>Homework helper</Typography>
         </Button>
 
-        <Button variant="contained" target='_blank' href='https://chat.openai.com/g/g-kLXy5ZCqD-logo-animator' style={{ fontFamily: "monospace", color: "black", background: "#ECDCB1", margin: "10px" }}>
-          <Typography variant='h7'>Logo Animator</Typography>
+        <Button variant="contained" target='_blank' href='https://chat.openai.com/g/g-kLXy5ZCqD-logo-animator' style={{ color: "black", background: "#ECDCB1", margin: "10px" }}>
+          <Typography variant='h6' style={{ fontFamily: "tektur" }}>Logo Animator</Typography>
         </Button>
 
-        <Button variant="contained" target='_blank' href='https://chat.openai.com/g/g-vfxEmHLfH-store-climber' style={{ fontFamily: "monospace", color: "black", background: "#ECDCB1", margin: "10px" }}>
-          <Typography variant='h7'>Store Climber</Typography>
+        <Button variant="contained" target='_blank' href='https://chat.openai.com/g/g-vfxEmHLfH-store-climber' style={{ color: "black", background: "#ECDCB1", margin: "10px" }}>
+          <Typography variant='h6' style={{ fontFamily: "tektur" }}>Store Climber</Typography>
         </Button>
       </Stack>
     </Box>
 
+
+    </Container>
+
+    <img src="/footer.png" alt="Banner" style={{ width: '100%', height: 'auto', display: 'block', paddingTop: "300px" }}/>
     </Container>
   );
 }
